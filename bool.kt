01dc31418b05b86f5fe7f00b41374a9ca96faba1
hemanthlanka9 <lankahemanthlokeshkumar@gmail.com>
2024-06-04 06:20:14 +0530

    add bool file to git

---
+++ bool.kt
@@ -0,0 +1,8 @@
+fun main()
+{
+    var mytrue=true
+    var myfalse=false
+    println(mytrue||myfalse) //This will be true
+    println(mytrue&&myfalse) //This will be false
+    println(!myfalse) //This will be true
+}
