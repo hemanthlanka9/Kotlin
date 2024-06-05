a7e68da38debb15b0a1f1da874b47582ebb7a4c8
hemanthlanka9 <lankahemanthlokeshkumar@gmail.com>
2024-06-05 10:39:03 +0530

    Adding the rockpaperscissors game to git

---
+++ rockpaperscissors.kt
@@ -0,0 +1,27 @@
+fun main(){
+    var computerChoice=""
+    var playerChoice=""
+    println("Rock, Paper or Scissors? Enter your choice:")
+    playerChoice=readln()
+    val randomNumber=(1..3).random()
+    if(randomNumber==1){
+        computerChoice="Rock"
+    }
+    else if(randomNumber==2){
+        computerChoice="Paper"
+    }
+    else{
+        computerChoice="Scissors"
+    }
+    println("Player Choice: "+playerChoice)
+    println("Computer Choice: "+computerChoice)
+
+    var winner=when{
+        playerChoice==computerChoice-> "Tie"
+        playerChoice=="Rock" && computerChoice=="Scissors"-> "Player"
+        playerChoice=="Paper" && computerChoice=="Rock"-> "Player"
+        playerChoice=="Scissors" && computerChoice=="Paper"-> "Player"
+        else->"Computer"
+    }
+    println("The winner is "+winner)
+}
