8b0f58b65ccd129ddcd8d4e851ba1a96fe62b49f
hemanthlanka9 <lankahemanthlokeshkumar@gmail.com>
2024-06-23 22:36:03 +0530

    changes in bookclass file

--- bookclass.kt
+++ bookclass.kt
@@ -1,15 +1,16 @@
-class Book(var title:String="Unknown",var author:String="Anonymous",var yearPublished:String="Current year")
-{
+class Book(
+        var title: String = "Unknown",
+        var author: String = "Anonymous",
+        var yearPublished: String = "Current year"
+) {}
 
-}
-fun main()
-{
-    var myBook=Book()
+fun main() {
+    var myBook = Book()
     println(myBook.title)
     println(myBook.author)
     print(myBook.yearPublished)
     println("\n")
-    var customBook=Book("Power","Robert Greene","1996")
+    var customBook = Book("Power", "Robert Greene", "1996")
     println(customBook.title)
     println(customBook.author)
     println(customBook.yearPublished)
